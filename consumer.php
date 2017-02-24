7d3ceb629048f1198ee8cce9888ae18f9fa59b58
Flc <3407725@qq.com>
2017-02-24 17:58:32 +0800

    init

---
+++ consumer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 任务执行者（常驻）
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+
+use App\Queue\Manager;
+use App\Jobs\Demo;
+
+if ('cli' !== php_sapi_name()) {
+    die('必须在命令行模式下运行');
+}
+
+while (true) {
+    // 从队列拉取任务
+    $job = Manager::instance()->pull();
+
+    // 如无任务，则休息2秒
+    if (! $job) {
+        sleep(2);
+        continue;
+    }
+
+    try {
+        call_user_func_array([$job, 'handle'], []);
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
---
+++ producer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 任务创建
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+
+use App\Queue\Manager;
+use App\Jobs\Demo;
+
+// 创建工作
+$demo = new Demo('测试');
+
+// 推送到队列
+for ($i = 0; $i <= 100; $i ++) {
+    Manager::instance()->push($demo);
+}
+
+echo Manager::instance()->count();
